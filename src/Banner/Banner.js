e858d5c2fca22750c2e9c217deadf00956cbd369
Iryna Shtynda <ira.shtynda@gmail.com>
2022-07-02 11:46:12 +0300

    added banner

---
+++ src/Banner/Banner.js
@@ -0,0 +1,17 @@
+import './Banner.css';
+
+function Banner() {
+    return (
+        <div>
+            <p>This website is under construction until August'19 but...</p>
+            <h1>We'll be back very soon</h1>
+            <p>and you still can order our services or subscribe for the best offers!</p>
+            <div>
+                <button>Our Services</button>
+                <button>Receive Offers</button>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
