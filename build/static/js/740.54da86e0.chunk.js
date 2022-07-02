5eaa42646f4187c22834875b315da14a00c4d4d4
Iryna Shtynda <ira.shtynda@gmail.com>
2022-07-02 23:08:23 +0300

    added build files

---
+++ build/static/js/740.54da86e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkleaf_company=self.webpackChunkleaf_company||[]).push([[740],{759:function(e,a,c){c.r(a),a.default={}}}]);
+//# sourceMappingURL=740.54da86e0.chunk.js.map
---
+++ build/static/js/759.a5a36e25.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkleaf_company=self.webpackChunkleaf_company||[]).push([[759],{756:function(e,a,c){c.r(a),a.default={}}}]);
+//# sourceMappingURL=759.a5a36e25.chunk.js.map
